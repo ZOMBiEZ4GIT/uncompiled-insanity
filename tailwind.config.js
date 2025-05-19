f8dbe8216a9129abe569217d93c55e6fdd0f6223
ZOMBiEZ4GIT <143467106+ZOMBiEZ4GIT@users.noreply.github.com>
2025-05-19 10:17:02 +1000

    fix: rename duplicate boxShadow key

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
         card: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
         'card-hover':
           '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-        card: '0 4px 24px 0 rgba(60, 40, 20, 0.15)',
+        'card-lg': '0 4px 24px 0 rgba(60, 40, 20, 0.15)',
       },
     },
   },
